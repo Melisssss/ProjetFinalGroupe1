78b9577f69fada91f20ecf379d4858c44f0a8299
Xavier <xpersil@gmail.com>
2017-10-13 00:11:21 +0200

    Voici ce à quoi je pensais en parlant de créer la classe famille, nous en parlerons demain si vous êtes d'accord ou pas

--- ProjetFinalServeur/src/main/java/com/g1/projetfinalserveur/dao/FicheImageDAO.java
+++ ProjetFinalServeur/src/main/java/com/g1/projetfinalserveur/dao/FicheImageDAO.java
@@ -1,9 +1,13 @@
 package com.g1.projetfinalserveur.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.g1.projetfinalserveur.metier.FicheImage;
 
-public interface FicheImageDAO extends JpaRepository<FicheImage, Long>{
-
+public interface FicheImageDao extends JpaRepository<FicheImage, Long> {
+	@Query("select f from FicheImage f where f.monEnfant.idEnfant = :x")
+	List<FicheImage> findAllFIcheImageEnfant(@Param("x") long idEnfant);
 }
--- ProjetFinalServeur/src/main/java/com/g1/projetfinalserveur/dao/FichePrincipaleDAO.java
+++ ProjetFinalServeur/src/main/java/com/g1/projetfinalserveur/dao/FichePrincipaleDAO.java
@@ -1,9 +1,14 @@
 package com.g1.projetfinalserveur.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.g1.projetfinalserveur.metier.FichePrincipale;
 
-public interface FichePrincipaleDAO extends JpaRepository<FichePrincipale, Long>{
-
+public interface FichePrincipaleDao extends JpaRepository<FichePrincipale, Long> {
+	@Query("select f from FichePrincipale f where f.monEnfant.idEnfant = :x")
+	List<FichePrincipale> findAllFichePrincipaleEnfant(@Param("x") long idEnfant);
 }
