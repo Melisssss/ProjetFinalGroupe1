7969e44adcf03436ce07328bd204d7de6c11f80c
Melisssss <melissa.perrot@outlook.com>
2017-10-12 12:15:34 +0200

    Base projet client complete

---
+++ ProjetFinalClient/WebContent/js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("myApp", []);
+
+//app.controller('liste', function($scope, $http) {
+//	$http.get("http://localhost:8080/springangulardata/all")
+//	.then(function(response) {
+//	$scope.produits = response.data;
+//	})
+//	.catch(function(reason){
+//		alert("Impossible de lire le fichier");
+//		console.log(reason);
+//	})
+//});
+
+
+  
