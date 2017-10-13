73a7be32d823ccb66298b774cd15f5e49864e3f5
Melisssss <melissa.perrot@outlook.com>
2017-10-13 19:57:11 +0200

    Derniere version index.html avec tests

--- ProjetFinalClient/WebContent/js/app.js
+++ ProjetFinalClient/WebContent/js/app.js
@@ -1,12 +1,24 @@
 var app=angular.module("myApp",[]);
+
+app.controller("authentificationResponsable", function($scope, $http){
+	$scope.responsable={};
+	$scope.authentifier=function(){
+		$http.post("http://localhost:8080/projetfinalserveur/getResponsable", $scope.responsable)
+		.then(function(response){
+			$scope.responsable=response.data;
+			})
+	}
+});
+
+
 app.controller("listEnfant",function($scope, $http){
-	$http.get("http://localhost:8080/projetfinalserveur")
+	$http.get("http://localhost:8080/projetfinalserveur/listEnfant")
     .then(function(response) {
         $scope.enfants = response.data;
        
     })
     .catch (function(reason){
-    	alert("erreur de récupération des données");
+    	alert("Erreur de récupération des données");
     	console.log(reason);	
     })
 });
@@ -16,11 +28,39 @@ app.controller("createEnfant", function($scope, $http) {
 	$scope.ajouter=function(){
 		$http.post("http://localhost:8080/projetfinalserveur/saveEnfant", $scope.enfant)
 		.then(function(value){
-			$scope.enfant=value;
+			$scope.enfant=response.data;
 		})
 		.catch(function(reason){
-			alert("Erreur rencontrée lors de l'ajout enfant");
+			alert("Erreur générée lors de l'ajout enfant");
 			console.log(reason);
 		})
 	}
 });
+app.controller("updateEnfant", function($scope, $http) {
+	$scope.enfant.monResponsable=$scope.responsable;
+	$scope.attribuerResponsabler=function(){
+		$http.post("http://localhost:8080/projetfinalserveur/updateEnfant", $scope.enfant)
+		.then(function(){
+		})
+		.catch(function(reason){
+			alert("Erreur générée lors de l'ajout enfant");
+			console.log(reason);
+		})
+	}
+});
+
+
+//app.controller("attribuer", function($scope, $http) {
+//	$scope.attribuerResponsable=function(){
+//		$http.post("http://localhost:8080/projetfinalserveur/linkEnfantResponsable", $scope.idEnfant, $scope.idUser)
+//		.then(function(){
+//			alert("Attribution ok");
+//		})
+//		.catch(function(reason){
+//			alert("Erreur générée lors de l'attribution d'un responsable à l'enfant");
+//			console.log(reason);
+//		})
+//	}
+//});
+
+//listEnfantResponsable
